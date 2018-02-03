9593958c71041f131ef76ea3c0762aeced38538f
Jason Kayser <jpk2f2@mail.missouri.edu>
2018-02-02 19:13:38 -0600

    First Commit.
    Forgot to use github for project, whoops

---
+++ src/checkerboard/BoardGenerator.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkerboard;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.AnchorPane;
+
+
+
+/**
+ *
+ * @author Jason
+ */
+public class BoardGenerator {
+    
+    private int numRows;
+    private int numCols;
+    private double boardHeight;
+    private double boardWidth;
+    private Color lightColor;
+    private Color darkColor;
+    
+    
+    
+    public BoardGenerator(int numRows, int numCol, double boardWidth, double boardHeight){
+        
+    }
+    
+    public BoardGenerator(int numRows, int numCol, double boardWidth, double boardHeight, Color lightColor, Color darkColor){
+        
+    }
+    
+    public AnchorPane build(){
+        
+    }
+    
+    public AnchorPane GetBoard(){
+        if(anchorPane != null)
+               return anchorPane;
+        return null;   
+    }
+    
+    
+}
---
+++ src/checkerboard/Startable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkerboard;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Jason
+ */
+public interface Startable {
+    public void start(Stage stage);
+}
